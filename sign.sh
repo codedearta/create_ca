b58dd1cf62475a11e25bdc386314455ad2661cd0
dearta <sepp.renfer@gmail.com>
2019-01-09 23:32:30 +0100

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -21,11 +21,26 @@ organizationName = optional
 organizationalUnitName = optional
 commonName = supplied" | sudo tee sign.cnf
 
+echo "
+basicConstraints=CA:FALSE
+subjectAltName=@san
+subjectKeyIdentifier = hash
+
+[san]
+DNS.1 = ${fqdn}
+DNS.2 = www.${fqdn}
+DNS.3 = dearta.ch
+DNS.4 = www.dearta.ch
+DNS.5 = dearta.co.uk
+DNS.6 = www.dearta.co.uk
+DNS.7 = dearta.london
+DNS.8 = www.dearta.london" | sudo tee ext.sign.cnf
+
 sudo mkdir newcerts
 sudo touch index.txt
 sudo echo '01' | sudo tee serial
 
-sudo openssl ca -config sign.cnf -out ${fqdn}.cert -infiles ${fqdn}.csr
+sudo openssl ca -config sign.cnf -extfile ext.sign.cnf -out ${fqdn}.cert -infiles ${fqdn}.csr
 
 cat ${fqdn}.key ${fqdn}.cert | sudo tee ${fqdn}.pem # use for mongod-tls
 cat ${fqdn}.crt ca.crt | sudo tee ${fqdn}.bundle.crt  # use for https
