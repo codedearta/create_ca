2602d92c593518f80efe823144959b8398c17bd6
dearta <sepp.renfer@gmail.com>
2019-01-09 23:11:38 +0100

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,2 +1,7 @@
 fqdn=dearta.com
-openssl x509 -req -in ${fqdn}.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out ${fqdn}.crt
+openssl x509 -req -sha512 -in ${fqdn}.csr -CA ca.cert -CAkey ca.key -CAcreateserial -out ${fqdn}.cert
+sudo openssl x509 -text -noout -in ${fqdn}.cert
+cat ${fqdn}.key ${fqdn}.cert | sudo tee ${fqdn}.pem # use for mongod-tls
+
+cat ${fqdn}.crt ca.crt | sudo tee ${fqdn}.bundle.crt  # use for https
+sudo openssl x509 -text -noout -in ${fqdn}.bundle.cert
